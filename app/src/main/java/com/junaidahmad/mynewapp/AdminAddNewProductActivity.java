3bc5d270195728a55b2faf915f76c61b3c259782
askjunaidahmad <64808292+askjunaidahmad@users.noreply.github.com>
2020-07-24 16:46:57 +0400

    Image in Product Page

--- app/src/main/java/com/junaidahmad/mynewapp/AdminAddNewProductActivity.java
+++ app/src/main/java/com/junaidahmad/mynewapp/AdminAddNewProductActivity.java
@@ -1,23 +1,71 @@
 package com.junaidahmad.mynewapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String CategoryName;
-    private TextView TestingText;
+    private Button ProductSaveButton;
+    private EditText InputProductName,InputProductDesc,InputProductPrice;
+    private ImageView ImgProduct;
+
+    private Uri ImgUri;
+
+    private static final int GalleryPic = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
 
 
-        TestingText = findViewById(R.id.simple_text);
         CategoryName = getIntent().getExtras().get("category").toString();
-        TestingText.setText(CategoryName);
+
+        ProductSaveButton = findViewById(R.id.btn_save_product);
+        InputProductName = findViewById(R.id.txt_product_name);
+        InputProductDesc = findViewById(R.id.txt_product_desc);
+        InputProductPrice =  findViewById(R.id.txt_product_price);
+        ImgProduct = findViewById(R.id.select_product_image);
+
+        ImgProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                OpenGallery();
+            }
+        });
+
+
+    }
+
+    private void OpenGallery()
+    {
+        Intent GalleryIntent = new Intent();
+        GalleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        GalleryIntent.setType("image/*");
+        startActivityForResult(GalleryIntent,GalleryPic);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GalleryPic && resultCode == RESULT_OK && data != null)
+        {
+            ImgUri = data.getData();
+            ImgProduct.setImageURI(ImgUri);
+        }
 
     }
 }
