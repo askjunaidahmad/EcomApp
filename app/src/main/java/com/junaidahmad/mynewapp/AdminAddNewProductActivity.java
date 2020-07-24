2608a5e71b0937d7fd065f6f4362ced8d30330e9
askjunaidahmad <64808292+askjunaidahmad@users.noreply.github.com>
2020-07-24 23:59:02 +0400

    update

--- app/src/main/java/com/junaidahmad/mynewapp/AdminAddNewProductActivity.java
+++ app/src/main/java/com/junaidahmad/mynewapp/AdminAddNewProductActivity.java
@@ -7,15 +7,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
-    private String CategoryName;
+    private String CategoryName,pName,pDesc,pPrice , saveCurrentTime, saveCurrentDate;
     private Button ProductSaveButton;
     private EditText InputProductName,InputProductDesc,InputProductPrice;
     private ImageView ImgProduct;
@@ -46,6 +51,13 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
             }
         });
 
+        ProductSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ValidateProductData();
+            }
+        });
+
 
     }
 
@@ -66,6 +78,49 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
             ImgUri = data.getData();
             ImgProduct.setImageURI(ImgUri);
         }
+
+    }
+
+    private void ValidateProductData()
+    {
+        pName = InputProductName.getText().toString();
+        pDesc = InputProductDesc.getText().toString();
+        pPrice = InputProductPrice.getText().toString();
+
+        if(ImgUri == null)
+        {
+            Toast.makeText(AdminAddNewProductActivity.this,"Please Select Product Image",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(pName))
+        {
+            Toast.makeText(AdminAddNewProductActivity.this,"Please Type Product Name",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(pDesc))
+        {
+            Toast.makeText(AdminAddNewProductActivity.this,"Please Type Product Description",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(pPrice))
+        {
+            Toast.makeText(AdminAddNewProductActivity.this,"Please Type Product Price",Toast.LENGTH_SHORT).show();
+        }
+        else
+            {
+                StoreProductInformation();
+            }
+
+    }
+
+    private void StoreProductInformation()
+    {
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd,yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+
 
     }
 }
